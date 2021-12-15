c65561a3b32e01e66a2cc57dd603ebddb333d61d
yunus <yunusy6@gmail.com>
2021-12-15 08:03:31 -0800

    Made changes to setting on django for static files

--- proj492/proj492/settings.py
+++ proj492/proj492/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+MEDIA_URL =  "/images/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
